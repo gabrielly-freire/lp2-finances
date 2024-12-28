05698db62cfc6690268cce15a00ad336d5906fa4
Gabrielly Freire <gabylopesfreire@gmail.com>
2024-12-27 21:20:16 -0300

    refactor: adição de atributos em person e equals e hashcode nos demais models

--- src/main/java/br/ufrn/imd/model/Bill.java
+++ src/main/java/br/ufrn/imd/model/Bill.java
@@ -4,6 +4,11 @@ import java.time.LocalDate;
 
 import br.ufrn.imd.model.enums.Category;
 
+/**
+ * Class that represents a bill.
+ * 
+ * @version 1.0
+ */
 public class Bill {
     
     private Long id;
@@ -87,6 +92,64 @@ public class Bill {
     public String toString() {
         return "Bill [description=" + description + ", value=" + value + ", dueDate=" + dueDate + ", paymentDate="
                 + paymentDate + ", isPaid=" + isPaid + ", category=" + category + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        result = prime * result + ((dueDate == null) ? 0 : dueDate.hashCode());
+        result = prime * result + ((paymentDate == null) ? 0 : paymentDate.hashCode());
+        result = prime * result + ((isPaid == null) ? 0 : isPaid.hashCode());
+        result = prime * result + ((category == null) ? 0 : category.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Bill other = (Bill) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        if (dueDate == null) {
+            if (other.dueDate != null)
+                return false;
+        } else if (!dueDate.equals(other.dueDate))
+            return false;
+        if (paymentDate == null) {
+            if (other.paymentDate != null)
+                return false;
+        } else if (!paymentDate.equals(other.paymentDate))
+            return false;
+        if (isPaid == null) {
+            if (other.isPaid != null)
+                return false;
+        } else if (!isPaid.equals(other.isPaid))
+            return false;
+        if (category != other.category)
+            return false;
+        return true;
     }
     
 }
--- src/main/java/br/ufrn/imd/model/Person.java
+++ src/main/java/br/ufrn/imd/model/Person.java
@@ -1,5 +1,141 @@
 package br.ufrn.imd.model;
 
-public class Person {
+import java.time.LocalDate;
+
+/**
+ * Class that represents a person.
+ * 
+ * @author Gabrielly Freire
+ * @version 1.0
+ */
+public abstract class Person {
     
+    private Long id;
+    private String name;
+    private String cpf;
+    private LocalDate birthDate;
+    private String address;
+    private String phone;
+
+    public Person() {
+    }
+
+    public Person(Long id, String name, String cpf, LocalDate birthDate, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [id=" + id + ", name=" + name + ", cpf=" + cpf + ", birthDate=" + birthDate + ", address="
+                + address + ", phone=" + phone + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + ((birthDate == null) ? 0 : birthDate.hashCode());
+        result = prime * result + ((address == null) ? 0 : address.hashCode());
+        result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        if (birthDate == null) {
+            if (other.birthDate != null)
+                return false;
+        } else if (!birthDate.equals(other.birthDate))
+            return false;
+        if (address == null) {
+            if (other.address != null)
+                return false;
+        } else if (!address.equals(other.address))
+            return false;
+        if (phone == null) {
+            if (other.phone != null)
+                return false;
+        } else if (!phone.equals(other.phone))
+            return false;
+        return true;
+    }
+
 }
